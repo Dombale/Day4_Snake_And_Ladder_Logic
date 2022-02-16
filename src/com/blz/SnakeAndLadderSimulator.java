8bfb5ad5a185940d79699eefac7e74f1cab46dbd
Priyanka <priyankadombale27@gmail.com>
2022-02-16 19:30:33 +0530

    Added: Welcome Message

--- src/com/blz/SnakeAndLadderSimulator.java
+++ src/com/blz/SnakeAndLadderSimulator.java
@@ -3,7 +3,7 @@ package com.blz;
 public class SnakeAndLadderSimulator {
 
 	public static void main(String[] args) {
-		System.out.println("  Welcome..!!");
+		System.out.println("  Welcome...!!");
 	}
 
 }
