ee325af109b10d1a3f0ea5bd6e06fe0121fbad38
Priyanka <priyankadombale27@gmail.com>
2022-02-16 19:35:50 +0530

    Added: Game starting at position 0 with single player

--- src/com/blz/SnakeAndLadderSimulator.java
+++ src/com/blz/SnakeAndLadderSimulator.java
@@ -4,6 +4,9 @@ public class SnakeAndLadderSimulator {
 
 	public static void main(String[] args) {
 		System.out.println("  Welcome...!!");
+		int startPosition = 0;
+
+		System.out.println(" Starting position of Snake and Ladder is : " + startPosition);
 	}
 
 }
