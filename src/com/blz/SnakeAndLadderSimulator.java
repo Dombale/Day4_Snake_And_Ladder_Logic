f5505360cd18706580c51b0cce4e61fbedf093cc
Priyanka <priyankadombale27@gmail.com>
2022-02-16 19:56:48 +0530

    Added: Rolling DICE between 1 to 6 by generating random number

--- src/com/blz/SnakeAndLadderSimulator.java
+++ src/com/blz/SnakeAndLadderSimulator.java
@@ -3,10 +3,13 @@ package com.blz;
 public class SnakeAndLadderSimulator {
 
 	public static void main(String[] args) {
-		System.out.println("  Welcome...!!");
-		int startPosition = 0;
-
+		System.out.println("  Welcome...!!"); // Print Welcome Message
+		int startPosition = 0; // Inialize variable for strt position with 0.
 		System.out.println(" Starting position of Snake and Ladder is : " + startPosition);
+
+		int randomNum = (int) Math.floor((Math.random() * 6) + 1);// Geanerate random Dice number between 1-6 using math
+																	// function.
+		System.out.println(" Dice Roll = " + randomNum);
 	}
 
 }
