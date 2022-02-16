7b8ea429bda7db49d7f5222665a851855dffbfcb
Priyanka <priyankadombale27@gmail.com>
2022-02-16 21:32:06 +0530

    Added: check no. of times die roll and position

--- src/com/blz/SnakeAndLadderSimulator.java
+++ src/com/blz/SnakeAndLadderSimulator.java
@@ -7,12 +7,14 @@ public class SnakeAndLadderSimulator {
 
 		int position = 0; // Initialize variable for start position of player with 0.
 		System.out.println(" Starting position of Snake and Ladder is : " + position);
-
+		int counter = 0;// Initialize variable for Counter value with 0.
 		while (position <= 100) {
 			int diceRoll = (int) Math.floor((Math.random() * 6) + 1);// Generate random Dice number between 1-6 using
 																		// math // function.
 			System.out.println(" Dice Roll = " + diceRoll);
 			int checkOption = (int) (Math.random() * 3); // Generate Random number check possible conditions
+
+			counter++;
 
 			switch (checkOption) { // Use switch case to check conditions.
 
@@ -36,7 +38,10 @@ public class SnakeAndLadderSimulator {
 			// if position beyond 100, then it remains stay on same position
 			if (position > 100)
 				position = position - diceRoll;
+			System.out.println("Dice number= " + diceRoll);
+			System.out.println("My position: " + position);
 		}
-		System.out.println("The position is: " + position);
+		System.out.println("Total number of times the dice was played= " + counter);
+
 	}
 }
